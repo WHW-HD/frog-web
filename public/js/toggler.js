967f84969d154c919dd615a11b7a1131a3ff62c7
alampert <a.lampert@gmx.de>
2018-03-20 18:54:56 +0100

    ostereier

---
+++ public/js/toggler.js
@@ -0,0 +1,26 @@
+// see index.mustache
+//
+const sunset = document.querySelector('#sunset')
+const forecast = document.querySelector('#vorhersage')
+
+const pressed = []
+const secretCode = ['sunset', 'forecast']
+const maxLength = secretCode.reduce((len, elem) => (elem.length > len ? elem.length : len), 0)
+
+window.addEventListener('keyup', (e) => {
+  pressed.push(e.key)
+  pressed.splice(-maxLength - 1, pressed.length - maxLength)
+  const index = secretCode.findIndex((elem) => pressed.join('').includes(elem))
+
+  switch (index) {
+  case 0:
+    sunset.classList.toggle('hidediv')
+    break
+  case 1:
+    forecast.classList.toggle('hidediv')
+    break
+  default:
+    break
+  }
+  console.log(pressed)
+})
