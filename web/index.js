9aeeaa1480e585e3eb7806340706c56a5f1c6502
alampert <a.lampert@gmx.de>
2018-05-03 21:21:56 +0200

    Moving Average für Windpfeil und Windgeschwindigkeit. Windgeschwindigkeit mit einer Tendenz

--- web/index.js
+++ web/index.js
@@ -2,6 +2,7 @@ import moment from 'moment'
 import Chart from 'chart.js'
 
 const chartDefinition = require('../lib/chart-definition')
+const MovingAverage = require('../lib/movingaverage_class')
 
 console.log('WHW ANEMOMETER')
 
@@ -10,15 +11,33 @@ socket.onopen = function(event) {
   socket.send('hello from ' + navigator.userAgent)
 }
 
+const av = []
+const mav = new MovingAverage(av, 100)
+const aa = []
+const maa = new MovingAverage(aa, 30)
+
 socket.onmessage = function(event) {
-  console.log('received data: ' + event.data)
   const data = event.data.split(':')
+
   if (data[0] == 'anemo/windvane') {
-    $('#windvane').css('transform', 'rotate(' + (parseFloat(data[1]) - 90) + 'deg)')
+    mav.add(parseFloat(data[1]))
+    console.log('MAV', mav.statistics())
+    $('#windvane').css('transform', 'rotate(' + (mav.average() - 90) + 'deg)')
+    //
   } else if (data[0] == 'anemo/anemo') {
-    $('#windspeed').html(parseFloat(data[1]).toFixed(2) + ' km/h')
+    maa.add(parseFloat(data[1]))
+    console.log('MAA', maa.statistics().tendency)
+    $('#windspeed').html(
+      maa.average().toFixed(1) +
+        ' km/h   (Tendenz:' +
+        maa.statistics().tendency.toFixed(1) +
+        ' km/h)'
+    )
+
+    //
   } else if (data[0] == 'anemo/rain') {
     $('#rain').html('Letzter Regen: ' + moment(parseInt(data[1])).fromNow())
+    //
   }
 }
 
@@ -30,10 +49,10 @@ const anemoChart = new Chart(anemoChartCtx, chartDefinition.anemoChartDefinition
 const vaneChart = new Chart(vaneChartCtx, chartDefinition.vaneChartDefinition)
 const rainChart = new Chart(rainChartCtx, chartDefinition.rainChartDefintion)
 
-console.log('anemochart', anemoChart)
+//console.log('anemochart', anemoChart)
 const updateCharts = () => {
   const updateInterval = 5000
-  console.log('anemochart', anemoChart)
+  //console.log('anemochart', anemoChart)
   let ic = 3
   jQuery.get(
     document.location.protocol + '//' + document.location.host + '/chartdata/anemo',
